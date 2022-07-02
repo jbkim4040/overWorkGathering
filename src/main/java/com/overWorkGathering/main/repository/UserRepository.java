8ef6a595da506c69bba3a031186a75d2dbe8b316
DESKTOP-HCJELPQ\jhyuk <jhyuk97@naver.com>
2022-07-02 21:18:07 +0900

    유저 repository수정

--- src/main/java/com/overWorkGathering/main/repository/UserRepository.java
+++ src/main/java/com/overWorkGathering/main/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.overWorkGathering.main.repository;
 
 import com.overWorkGathering.main.entity.UserEntity;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, String> {
 	UserEntity findByUserIdAndPw(String userId, String pw);
 	UserEntity findByUserId(String userId);
+	List<UserEntity> findAllByPart(String part); 
 }
