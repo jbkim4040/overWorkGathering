0edfd075dd419034e84feadf8ea69af624f0eac3
jbkim4040 <jbkim404037@gmail.com>
2022-12-13 12:23:31 +0900

    젠킨스 빌드를 위한 bootJar, bootRun 추가

--- src/main/java/com/overWorkGathering/main/Application.java
+++ src/main/java/com/overWorkGathering/main/Application.java
@@ -2,11 +2,19 @@ package com.overWorkGathering.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing  // JPA Auditing 활성화
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
