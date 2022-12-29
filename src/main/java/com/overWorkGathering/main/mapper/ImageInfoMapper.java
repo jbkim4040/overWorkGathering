a29cc84aa13f0a5f08066ff2d95a4450d5c8f5bd
gimjeongbin <jbkim404037@gmail.com>
2022-12-29 22:46:09 +0900

    수정

--- src/main/java/com/overWorkGathering/main/mapper/ImageInfoMapper.java
+++ src/main/java/com/overWorkGathering/main/mapper/ImageInfoMapper.java
@@ -3,8 +3,6 @@ package com.overWorkGathering.main.mapper;
 import com.overWorkGathering.main.DTO.ImageInfoDTO;
 import com.overWorkGathering.main.entity.ImageInfoEntity;
 
-import java.util.List;
-
 public interface ImageInfoMapper {
 
     ImageInfoEntity toImageEntity (ImageInfoDTO imageInfoDTO);
